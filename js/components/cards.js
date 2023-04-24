c752814e1321dbbe6aa376ff03bb873d9f1cf646
StivenCarvajalCampus <StivenCarvajal3211@gmail.com>
2023-04-24 09:37:07 -0500

    feat(pokeapi): :sparkles: cambios en workers

--- js/components/cards.js
+++ js/components/cards.js
@@ -1,8 +1,9 @@
 export default {
     async setData() {
         const ws = new Worker('js/workers/wsMyPokemon.js', {type:'module'})
-        ws.postMessage({id:54})
+        ws.postMessage({id:98})
         ws.addEventListener('message', r => { 
+            console.log(r.data)
             document.querySelector("#template").insertAdjacentHTML("beforeend",r.data)
         })
     }
--- js/workers/wsMyPokemon.js
+++ js/workers/wsMyPokemon.js
@@ -1,46 +1,51 @@
-export let wsPokemon = {
-    async fetchData(id){
+const pokeapi = async()=>{
         try {
-            const res = await fetch (`https://pokeapi.co/api/v2/pokemon/${id}`)
+            const res = await fetch (`https://pokeapi.co/api/v2/pokemon/?offset=0&limit=1281`)
             const data = await res.json()
+            let results =""
+            for (let i = 0; i < 120; i++) {
+                const pokemon = await fetch(data.results[i].url);
+                const datap = await pokemon.json();
+                
+            
             let plantilla = `
             <article class="card">
             <img src="./images/bg-pattern-card.svg" alt="imagen header card" class="card-header">
             <div class="card-body">
-                <img src=${data.sprites.other.dream_world.front_default} alt="imagen de vitoko" class="card-body-img">
+                <img src=${datap.sprites.other.dream_world.front_default} alt="imagen de vitoko" class="card-body-img">
                 <h1 class="card-body-title">
-                    ${data.name}
-                    <span> <p>Experience </p>${data.base_experience}</span>
+                    ${datap.name}
+                    <span> <p>Experience </p>${datap.base_experience}</span>
                 </h1>
                 <p class="card-body-text"></p>
             </div>
             <div class="card-footer">
                 <div class="card-footer-social">
-                    <h3>80K</h3>
-                    <p>Followers</p>
+                    <h3>${datap.moves[0].move.name}</h3>
+                    <p>Moves</p>
                 </div>
                 <div class="card-footer-social">
-                    <h3>803K</h3>
-                    <p>Likes</p>
+                    <h3>${datap.stats[0].base_stat}</h3>
+                    <p>Stats</p>
                 </div>
                 <div class="card-footer-social">
-                    <h3>1.4K</h3>
-                    <p>Photos</p>
+                    <h3>${datap.stats[0].stat.name}</h3>
+                    <p>Name Stat</p>
                 </div>
             </div>
         </article>`;
-            return plantilla
+        results+=(plantilla)
+        }   
+
+    return results
+
         } catch (error) {console.log(error)}
     }
-}
 
 
-self.addEventListener('message', event => {
-    const data = event.data;
-    wsPokemon.fetchData(data.id).then(res => {
-        postMessage(res)
-
-    }) 
+self.addEventListener('message',async event => {
+    let resultados= await pokeapi();
+    self.postMessage(resultados);
 })
 
 
