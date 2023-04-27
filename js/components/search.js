210055bd778d2c50e3548048b3fa0dbdd96c74b0
StivenCarvajalCampus <124050749+StivenCarvajalCampus@users.noreply.github.com>
2023-04-27 08:45:30 -0500

    Update search.js

--- js/components/search.js
+++ js/components/search.js
@@ -2,7 +2,7 @@ export default {
     async setDataSearch(){    
         document.querySelector("#buscar").addEventListener("click",()=>{
             let val= document.querySelector("#busqueda").value;
-            const worker = new Worker('../js/workers/wsMySearch.js');
+            const worker = new Worker('./js/workers/wsMySearch.js');
             worker.postMessage({val:val});
             worker.onmessage = function(e) {
                 console.log(e.data)
